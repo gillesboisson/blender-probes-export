4c80705a8afec7ff28f2b080b91326d5afdd0294
Gilles Boisson <gilles@scopopop.com>
2023-10-05 11:18:43 +0200

    fix cube mapping and coordinate for webgl

--- probes/helpers/config.py
+++ probes/helpers/config.py
@@ -14,24 +14,25 @@ cube_map_face_names = [
 
 cube_map_euler_rotations = [
     
-    (pi / 2, 0, pi / 2),
     (pi / 2, 0, -pi / 2),
-    (pi / 2, 0, pi), 
+    (pi / 2, 0, pi / 2),
     (pi / 2, 0, 0), 
-    (0, 0, 0),
+    (pi / 2, 0, pi), 
     (pi, 0, 0),
+    (0, 0, 0),
+    
 ]
 
 
 pano_cube_map_euler_rotations = [
     
-    (pi / 2, 0, pi / 2),
     (pi / 2, 0, -pi / 2),
-    (0, pi / 2, pi / 2),
+    (pi / 2, 0, pi / 2),
     (0, -pi / 2, -pi / 2),
-    (pi / 2, 0, pi),  
+    (0, pi / 2, pi / 2),
     (pi / 2, 0, 0),
-
+    (pi / 2, 0, pi),  
+    
 ]
 
 
